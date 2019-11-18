5626e5f133cc9d50f3c44ad00b829f9e491e3d3f
梁慧燃 <lianghr@sq580.com>
2019-11-18 14:28:43 +0800

    Unique Paths II

--- L-Unique-Paths.js
+++ L-Unique-Paths.js
@@ -1,4 +1,5 @@
 // https://leetcode-cn.com/problems/unique-paths/
+// 升级版 https://leetcode-cn.com/problems/unique-paths-ii/
 
 /*
     dp[i][j] = dp[i - 1][j] + dp[i][j - 1];     // i >= 0 && i < n && j >=0 && j < m;
@@ -19,4 +20,35 @@ const uniquePaths = (m, n) => {
         }
     }
     return dp[n - 1][m - 1];
+};
+
+/**
+ *
+ * @param {number[][]} grid
+ * @return {number}
+ */
+
+// dp[i][j] = grid[i][j] ? 0 : dp[i + 1][j] + dp[i][j + 1]
+// 可以在最外层设置为dp[n][0~m] = 0; dp[0~n][m] = 0;
+const uniquePathsWithObstacles = grid => {
+    let n = grid.length;
+    let m = grid[0].length;
+    let dp = [];
+    for(let i = n - 1; i >= 0; i--){
+        dp[i] = [];
+        for(let j = m - 1; j >= 0; j--){
+            if(grid[i][j]) dp[i][j] = 0;
+            else if(i + 1 === n && j + 1 === m) dp[i][j] = 1;
+            else {
+                if(j + 1 === m){
+                    dp[i][j] = dp[i + 1][j];
+                } else if(i + 1 === n){
+                    dp[i][j] = dp[i][j + 1];
+                } else {
+                    dp[i][j] = dp[i][j + 1] + dp[i + 1][j];
+                }
+            }
+        }
+    }
+    return dp[0][0];
 };
