60046797d950c4026b69e9694a2b837b3e0610ac
梁慧燃 <lianghr@sq580.com>
2019-11-22 16:56:02 +0800

    Pre-Order-Traversal 前序遍历

---
+++ L-Pre-Order-Traversal.js
@@ -0,0 +1,42 @@
+// https://leetcode-cn.com/problems/binary-tree-preorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {Array} traversal
+ * @return {number[]}
+ */
+
+//递归算法
+const preorderTraversal = (root, traversal = []) => {
+    if(!root) return traversal;
+    traversal.push(root.val);
+    if(root.left) preorderTraversal(root.left, traversal);
+    if(root.right) preorderTraversal(root.right, traversal);
+    return traversal;
+};
+
+//归纳算法
+
+const preorderTraversal1 = root => {
+    let traversal = [];
+    if(!root) return traversal;
+    let stack = [];
+    let cur = root;
+    while(cur !== null || stack.length !== 0){
+        while(cur !== null){
+            traversal.push(cur.val);
+            stack.push(cur);
+            cur = cur.left;
+        }
+        let node = stack.pop();
+        cur = node.right;
+    }
+    return traversal;
+};
