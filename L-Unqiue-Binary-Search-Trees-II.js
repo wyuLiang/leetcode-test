bfebc384773d32bd00c1560a5c96459370027610
梁慧燃 <lianghr@sq580.com>
2019-11-26 11:29:53 +0800

    remove

--- L-Unqiue-Binary-Search-Trees-II.js
+++
@@ -1,9 +0,0 @@
-// https://leetcode-cn.com/problems/unique-binary-search-trees-ii/
-
-
-// dp[n] = dp[n - 1] + null + n
-// dp[n] = n + dp[n - 1]
-const generateTrees = n => {
-    if(n === 1) return [1];
-
-};
